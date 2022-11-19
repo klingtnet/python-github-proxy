b9f4066ab39f6b1304d71972d5b07739ab773fe4
Andreas Linz <klingt.net@gmail.com>
2022-11-19 15:40:24 +0100

    Allow to set base URL and pass in environment
    Both changes are beneficial for testing because mocking the environment
    is pretty complicated and with a custom base URL we can mock the GitHub
    API with an actual HTTP server.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import argparse
 import logging
 import sys
-from typing import List
+from typing import List, Dict
 import os
 
 import client
@@ -9,19 +10,29 @@ import client
 GITHUB_TOKEN_KEY = "GITHUB_TOKEN"
 
 
-def main(args: List[str]):
+def main(args: List[str], environ: Dict[str, str]):
     # Nothing will be logged if basic config wasn't called once.
     # Not the best logging API out there.
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(__name__)
 
-    github_token = os.environ.get(GITHUB_TOKEN_KEY)
+    github_token = environ.get(GITHUB_TOKEN_KEY)
     if not github_token:
         log.critical(f"{GITHUB_TOKEN_KEY} required")
+        sys.exit(1)
 
-    gh = client.GitHub(github_token, logger=log)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--github-base-url", help=f"Base URL of GitHub REST API.")
+    args = parser.parse_args(args)
+    if args.github_base_url:
+        gh = client.GitHub(github_token, logger=log, base_url=args.github_base_url)
+    else:
+        gh = client.GitHub(github_token, logger=log)
+
     gh.verify()
+
+    # TODO: Implement server which exposes some client methods.
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:], dict(os.environ))
